df8fe9a2455d3d5ced9f6075e28283953dea26e9
Arpit Mohapatra <arpitmohapatra18@gmail.com>
2026-01-24 00:40:06 -0500

    Updated Password

--- backend/auth.py
+++ backend/auth.py
@@ -1,7 +1,9 @@
 import os
 from fastapi import HTTPException
 
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "dosa123")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+if not ADMIN_PASSWORD:
+    raise ValueError("ADMIN_PASSWORD environment variable is not set")
 
 def verify_admin(password: str):
     if password != ADMIN_PASSWORD:
